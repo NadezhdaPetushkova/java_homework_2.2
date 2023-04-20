f801cb1357a4ece6bbc6a15c989f4ce2059dba48
Petushkova Nadezhda <nadin202@mail.ru>
2023-04-20 13:45:56 +0300

    Homework 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balans = 50;
-        int popolnenie = 1300;
+        int balance = 50;
+        int refill = 1300;
         int bonus;
-        int summa;
-        if (popolnenie > 1000) {
-            bonus = popolnenie / 100;
-        } else bonus = 0;
-        summa = balans + popolnenie + bonus;
+        int amount;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        amount = balance + refill + bonus;
         System.out.println("Количество бонусных рублей = " + bonus);
-        System.out.println("Итоговая сумма баланса = " + summa);
+        System.out.println("Итоговая сумма баланса = " + amount);
     }
 }
